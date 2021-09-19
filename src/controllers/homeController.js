66a8c65d0b98b38817851162ce3843b81f7642ef
ADMIN\Admin <xuanhoang997@gmail.com>
2021-09-19 18:05:03 +0700

    update data use ORM

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -37,9 +37,33 @@ let displayGetCRUD = async(req, res) => {
     });
 }
 
+let getEditCRUD = async(req, res) => {
+    let userId = req.query.id;
+    console.log(userId);
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        // check user data not Found
+
+        return res.render('editCRUD.ejs', {
+            user: userData,
+        });
+    } else {
+        return res.send('Users not found');
+    }
+}
+
+let putCRUD = async(req, res) => {
+    let data = req.body;
+    await CRUDService.updateUserData(data);
+    return res.send('update done');
+
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
