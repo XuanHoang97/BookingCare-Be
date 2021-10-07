a471c03866a0111b4bf86f7a03a3999eb0aeb16c
ADMIN\Admin <xuanhoang997@gmail.com>
2021-10-07 15:38:14 +0700

    add API Schedule

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -59,9 +59,24 @@ let getDetailDoctorById = async(req, res) => {
     }
 }
 
+//create schedule time doctor
+let bulkCreateSchedule = async(req, res) => {
+    try {
+        let infor = await doctorService.bulkCreateSchedule(req.body);
+        return res.status(200).json(infor);
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     postInforDoctor: postInforDoctor,
-    getDetailDoctorById: getDetailDoctorById
+    getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule
 }
